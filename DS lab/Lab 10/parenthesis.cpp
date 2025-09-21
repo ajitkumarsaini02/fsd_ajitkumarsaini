1e3e42c47908edf8365d0cad305b0cc42e793b2f
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2025-09-21 23:14:12 +0530

    ...

--- DS lab/Lab 10/parenthesis.cpp
+++ DS lab/Lab 10/parenthesis.cpp
@@ -1,52 +1,9 @@
 #include<iostream>
 #define STACKSIZE 10
+#include "CharStackHeader.h"
 using namespace std;
 
-struct stack {
-    char item[STACKSIZE];
-    int top;
-} S;
 
-void Initialize() {
-    S.top = -1;
-}
-
-bool IsEmpty() {
-    if (S.top == -1) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-void push(char x) {
-    if (S.top == STACKSIZE - 1) {
-        cout << "Stack overflow" << endl;
-        exit(1);
-    } else {
-        S.top += 1;
-        S.item[S.top] = x;
-    }
-}
-
-char pop() {
-    if (IsEmpty()) {
-        cout << "Stack underflow" << endl;
-        exit(1);
-    } else {
-        char x = S.item[S.top];
-        S.top -= 1;
-        return x;
-    }
-}
-
-char stackTop() {
-    if (IsEmpty()) {
-        cout << "Stack is empty" << endl;
-        exit(1);
-    }
-    return S.item[S.top];
-}
 void balanceParenthesis(char exp[]){
     Initialize();
     int i=0,flag=0;
